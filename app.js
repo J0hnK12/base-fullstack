bf4aa5568d379c309236d63dce3af0a9c8d82991
John Word <jword@sylvaniaschools.local>
2018-12-03 08:43:42 -0500

    Monday 2nd period

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/',(req, res) => {
     res.send('Hello!');
 });
+
+app.post('/answer',(req, res) => {
+    res.send("Thank you for your answer!");
+    console.log(req.body);
+})
 
 app.listen(3000, () => {
     console.log('Server started...');
