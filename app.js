cd8380edf35dba093c90d138a0c75b3965703007
J0hnK12 <43782002+J0hnK12@users.noreply.github.com>
2019-02-25 08:48:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 let eventsData = []; //readfile;
 let eventList = eventsData.map(eventsData => eventsData.Event);
 
+fs.readFile('db.json', (err, data) => {
+    videoGame = JSON.parse(data);
+});
+
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -13,7 +17,14 @@ app.get('/',(req, res) => {
 
 app.post('/answer',(req, res) => {
     res.send("Thank you for your answer!");
-    console.log(`IP address ${req.ip} voted for ${req.body.events}`);    
+    console.log(`IP address ${req.ip} voted for ${req.body.events}`); //Have the console save the first name, last name, student ID, grade, and event
+    events.push(req.body.events);
+    fs.writeFile('db.json', JSON.stringify(events), (err) => {
+        console.log(events);
+        console.warn(err);
+        console.log(db.json);
+    });
+    
 })
 
 app.listen(3000, () => {
