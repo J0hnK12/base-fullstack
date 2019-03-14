c7167c7a28b50756c1b72bb31970eab513d88477
J0hnK12 <43782002+J0hnK12@users.noreply.github.com>
2019-03-14 09:48:00 -0400

    Update playground.js

--- playground.js
+++ playground.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 let students = [];
 fs.readFile('assignments.json', async (err, data) => {
-students = await JSON.parse(data);
-let event = "Just Dance";
-
-console.log("List of students in Just Dance"); 
+    students = await JSON.parse(data);
+    let event = "Just Dance";
+    console.log("List of students in Just Dance"); 
 })
