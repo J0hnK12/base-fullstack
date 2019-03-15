eac3149bd5f09a502f7ba1a0507e402b42ac784c
J0hnK12 <43782002+J0hnK12@users.noreply.github.com>
2019-03-15 07:49:31 -0400

    Update playground.js

--- playground.js
+++ playground.js
@@ -4,6 +4,15 @@ let students = [];
 fs.readFile('assignments.json', async (err, data) => {
     students = await JSON.parse(data);
     let event = "Just Dance";
+    for (let i = 0; i < students.length; i++) {
+        if (students == justDance) {
+            events.append("Just Dance");
+        }
+        
+        else {
+            
+        }
+    }
     
     console.log("List of students in Just Dance"); 
 })
